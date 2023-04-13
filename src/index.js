c5c4f202ec1bcd6c9eabf8b34e71c4e43f295a26
matheusbruns <matheus.rbruns@gmail.com>
2023-04-13 00:05:58 -0300

    TAXBRAIN-17 desenvolvimento dos endpoints de cadastro e verificação de login de usuário

--- src/index.js
+++ src/index.js
@@ -1,26 +1,20 @@
 const express = require('express'); // Importa o módulo do Express
 const app = express(); // Cria uma instância do Express
-require('./config/dbConfig');
+const database = require('./config/dbConfig');
 const session = require('express-session');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const cors = require('cors');
-const routes = require('./routes');
+const routes = require('./routes/routes');
 
 var path = require('path');
 
-
-
-
-
-
-
-
-
 const PORT = 5000; // Define a porta na qual o servidor deve escutar
 
-app.use(session({ secret: 'kasdakhdjakdajhkjhaksdjhakdjhakdjhkajhsd' }));
 app.use(express.json());
+app.use("/api/users", routes);
+
+app.use(session({ secret: 'kasdakhdjakdajhkjhaksdjhakdjhakdjhkajhsd' }));
 app.use(routes);
 app.use(fileUpload({
   useTempFiles: true,
@@ -31,3 +25,4 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Servidor escutando na porta ${PORT}`);
 });
+
